61f09c3201ac38ef38590d56641d1fa95dfc5295
Muneeb Ahmad <muneeb.ahmad0600>
2019-01-15 14:30:54 +0500

    commit from dev

--- app/src/main/java/com/example/muneebahmad/gittestapp/MainActivity.java
+++ app/src/main/java/com/example/muneebahmad/gittestapp/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.muneebahmad.gittestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etName, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editText);
+        etPassword = findViewById(R.id.editText2);
+        btnLogin = findViewById(R.id.button);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Yolo",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
